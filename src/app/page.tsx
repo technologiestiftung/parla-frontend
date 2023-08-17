ca683dbbb9bbda385f72239c93dd3f049f7c6099
ff6347 <hash@ff6347.email>
2023-08-17 12:02:29 +0200

    refactor(next): move all code to next
    Add an error in userland where I thought it was astro

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import Image from 'next/image'
+import Search from "./components/Search"
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+
+
+
+    <Search />
+
+    </main>
+  )
+}
--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,8 +1,11 @@
 import { createClient } from "@supabase/supabase-js";
 import type { Database } from "./database";
+import { EnvError } from "./errors";
 
-const supabase = createClient<Database>(
-	import.meta.env.SUPABASE_URL,
-	import.meta.env.SUPABASE_ANON_KEY
-)
-export default supabase
+const SUPABASE_URL = process.env.SUPABASE_URL;
+const SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY;
+if (SUPABASE_URL === undefined) throw new EnvError("SUPABASE_URL is undefined");
+if (SUPABASE_ANON_KEY === undefined)
+	throw new EnvError("SUPABASE_ANON_KEY is undefined");
+const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_ANON_KEY);
+export default supabase;
