c7be5225819b3648f14ca9fc9498efa42002d3d0
ff6347 <hash@ff6347.email>
2023-08-23 18:20:47 +0200

    chore: Housekeeping

--- src/components/SearchResult.tsx
+++ src/components/SearchResult.tsx
@@ -25,43 +25,44 @@ export default function SearchResult({ result }: SearchResultProps) {
 					<h2 className="text-2xl py-2 text-left">referenzierte Daten</h2>
 					<ul className="list-none text-left w-full">
 						{result.sections.map((section) => {
-							if (!section.pdfs) return null;
-							const { desk, titel, lokurl } = section.pdfs[0];
-							const { content } = section;
-							const pdfFilename = lokurl
-								?.split("/")
-								.findLast((str) => str.endsWith(".pdf"));
-
+							const { content, similarity } = section;
 							return (
 								<li key={section.id} className="py-4 pb-8 overflow-x-auto">
 									<table className="table-auto min-w-full">
-										{section.pdfs.map((pdf) => (
-											<tbody key={pdf.id}>
-												<tr>
-													<td>Titel:</td>
-													<td>{titel}</td>
-												</tr>
-												<tr>
-													<td>PDF:</td>
-													<td>
-														<a
-															target="_blank"
-															rel="noreferrer"
-															href={pdf.lokurl ? pdf.lokurl : ""}
-															className="underline text-blue-500 hover:text-blue-800 visited:text-blue-950"
-														>
-															{pdfFilename}
-														</a>
-													</td>
-												</tr>
+										{section.pdfs &&
+											section.pdfs.map((pdf) => (
+												<tbody key={pdf.id}>
+													<tr>
+														<td>Titel:</td>
+														<td>{pdf.titel}</td>
+													</tr>
+													<tr>
+														<td>PDF:</td>
+														<td>
+															<a
+																target="_blank"
+																rel="noreferrer"
+																href={pdf.lokurl ? pdf.lokurl : ""}
+																className="underline text-blue-500 hover:text-blue-800 visited:text-blue-950"
+															>
+																{pdf.lokurl
+																	?.split("/")
+																	.findLast((str) => str.endsWith(".pdf"))}
+															</a>
+														</td>
+													</tr>
 
-												<tr>
-													<td>Beschreibung:</td>
-													<td>{desk}</td>
-												</tr>
-											</tbody>
-										))}
+													<tr>
+														<td>Beschreibung:</td>
+														<td>{pdf.desk}</td>
+													</tr>
+												</tbody>
+											))}
 										<tbody>
+											<tr>
+												<td>Similarität:</td>
+												<td>{similarity}</td>
+											</tr>
 											<tr>
 												<td>Kontext</td>
 												<td>{content}</td>
