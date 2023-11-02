3378fffe981a46def2a97be47f9a37e07eb79e37
Jonas Jaszkowic <jonas.jaszkowic@ts.berlin>
2023-11-02 14:23:29 +0100

    chore: adjust splash screen text

--- src/components/splash-screen.tsx
+++ src/components/splash-screen.tsx
@@ -8,7 +8,7 @@ import {
 	Dialog,
 	DialogContent,
 	DialogHeader,
-	DialogTitle
+	DialogTitle,
 } from "@/components/ui/dialog";
 import { Link } from "./Link";
 
@@ -29,16 +29,20 @@ export function SplashScreen({
 			>
 				<DialogHeader className="flex justify-center mb-4">
 					<DialogTitle className="text-blue-400">
-						KI Assistent für Schriftliche Anfragen
+						KI Assistent für Schriftliche Anfragen und Rote Nummern
 					</DialogTitle>
 				</DialogHeader>
 				<div className="space-y-3 text-sm">
 					<p>
-						Dieser Prototyp durchsucht die Schriftlichen Anfragen der
-						Wahlperiode 19 des Berliner Abgeordnetenhauses und erstellt anhand
-						eines Large Language Models Vorlagen für die Beantwortung der
-						gestellten Frage. Die Quellen sind auf
-						parlament-berlin.de/dokumente/open-data zu finden.
+						Dieser Prototyp durchsucht die Schriftlichen Anfragen und die
+						Hauptausschussprotokolle ("Rote Nummern") der Wahlperiode 19 des
+						Berliner Abgeordnetenhauses und erstellt anhand eines Large Language
+						Models Vorlagen für die Beantwortung der gestellten Frage. Die
+						Quellen sind auf{" "}
+						<a href="https://www.parlament-berlin.de/dokumente">
+							parlament-berlin.de/dokumente
+						</a>{" "}
+						zu finden.
 					</p>
 					<p>
 						Bitte beachten Sie, dass die generierten Antworten zwar aus
