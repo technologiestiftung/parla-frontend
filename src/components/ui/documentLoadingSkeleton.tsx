c3e17af005769ea4a09a4812393fcc50f2d80509
Lucas Vogel <contact@vogelino.com>
2023-11-27 10:51:53 -0300

    refactor(TextLoadingSkeleton): Fix width of loading skeleton lines

--- src/components/ui/documentLoadingSkeleton.tsx
+++ src/components/ui/documentLoadingSkeleton.tsx
@@ -43,7 +43,6 @@ function DocumentLoadingSkeletonItem({
 							key={idx}
 						/>
 					))}
-				T
 			</div>
 		</div>
 	);
--- src/components/ui/textLoadingSkeleton.tsx
+++ src/components/ui/textLoadingSkeleton.tsx
@@ -1,18 +1,21 @@
 import React from "react";
 
 function TextLoadingSkeleton({ linesCount = 10, animate = true }) {
+	const offsets = [30, 20, 40, 30, 50, 40, 20, 10, 30, 20];
 	return (
 		<div className="flex flex-col gap-2">
 			{[...Array(linesCount)].map((_, i) => {
-				const widthOffset = Math.floor(Math.random() * 10) * 10;
-				console.log(widthOffset);
+				const offset = offsets[i % offsets.length];
 				return (
 					<span
 						key={i}
 						className={`${
 							animate ? "animate-bounce" : ""
-						} bg-slate-200 h-3 w-[calc(100%-${widthOffset}px)] block`}
-						style={{ animationDelay: `${i * 40}ms` }}
+						} bg-slate-200 h-3 block`}
+						style={{
+							animationDelay: `${i * 40}ms`,
+							width: `calc(100% - ${offset}px)`,
+						}}
 					/>
 				);
 			})}
