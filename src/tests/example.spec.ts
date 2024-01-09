be3e57eb7b522d70d30a74fc26c365a8db951149
Jonas Jaszkowic <jonas.jaszkowic@ts.berlin>
2024-01-09 11:39:12 +0100

    chore: init playwright tests

---
+++ src/tests/example.spec.ts
@@ -0,0 +1,6 @@
+import { test, expect } from "@playwright/test";
+
+test("has title", async ({ page }) => {
+	await page.goto("http://localhost:3000");
+	await expect(page).toHaveTitle("Parla");
+});
