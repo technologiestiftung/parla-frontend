1263bb5940935cbe2e22e26d66be28a5835946ae
Jonas Jaszkowic <jonas.jaszkowic@ts.berlin>
2024-01-15 14:32:11 +0100

    feat: opengraph and twitter meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,44 @@ import "./globals.css";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-	title: "Parla",
-	description: "Pardok durchsuchbar",
+	openGraph: {
+		title: "Parla",
+		description: "KI-Assistent für Schriftliche Anfragen und Rote Nummern",
+		url: "https://parla.berlin",
+		siteName: "Parla",
+		images: [
+			{
+				url: "/images/logo.png",
+				width: 800,
+				height: 600,
+			},
+			{
+				url: "/images/logo.png",
+				width: 1800,
+				height: 1600,
+				alt: "Parla Logo",
+			},
+		],
+		locale: "en_US",
+		type: "website",
+	},
+	twitter: {
+		title: "Parla",
+		description: "KI-Assistent für Schriftliche Anfragen und Rote Nummern",
+		images: [
+			{
+				url: "/images/logo.png",
+				width: 800,
+				height: 600,
+			},
+			{
+				url: "/images/logo.png",
+				width: 1800,
+				height: 1600,
+				alt: "Parla Logo",
+			},
+		],
+	},
 };
 
 export default function RootLayout({
