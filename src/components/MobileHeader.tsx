0a6f88d6ee92bf239ceb36403d1365e534e6b8f0
Jonas Jaszkowic <jonas.jaszkowic@ts.berlin>
2024-01-15 10:40:41 +0100

    fix: wrong property in header

--- src/components/MobileHeader.tsx
+++ src/components/MobileHeader.tsx
@@ -4,7 +4,6 @@ export default function MobileHeader({
 	setSidebarisOpen,
 	openSplashScreen,
 }: {
-	sidebarIsOpen: boolean;
 	setSidebarisOpen: (isOpen: boolean) => void;
 	openSplashScreen: () => void;
 }) {
