a2ab2ab2505ce703c67c942b606cdb99b4ba2d39
Jonas Jaszkowic <jonas.jaszkowic@ts.berlin>
2024-01-17 11:35:18 +0100

    feat: opengraph and twitter meta (#66)
    * feat: opengraph and twitter meta
    
    * chore: add more metadata tags
    
    * feat: use final logo for openGraph and twitter metadata
    
    * chore: add image alt
    
    * fix: use images with correct aspect ratio

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,53 @@
 import "./globals.css";
 import type { Metadata } from "next";
 
+const title = "Parla";
+const description = "KI-Assistent für Schriftliche Anfragen und Rote Nummern";
+const url = "https://parla.berlin";
+
 export const metadata: Metadata = {
-	title: "Parla",
-	description: "Pardok durchsuchbar",
+	title: title,
+	applicationName: url,
+	description: description,
+	keywords: [
+		"Parla",
+		"KI",
+		"Berliner Senat",
+		"Abgeordnetenhaus",
+		"Mitbeteiligung",
+		"Transparenz",
+		"Dokumentensuche",
+	],
+	authors: { url: "https://citylab-berlin.org/", name: "CityLab Berlin" },
+	openGraph: {
+		title: title,
+		siteName: url,
+		description: description,
+		url: url,
+		images: [
+			{
+				url: "/images/parla-logo-og-image.png",
+				width: 1200,
+				height: 629,
+				alt: "Parla Logo",
+			},
+		],
+		locale: "de_DE",
+		type: "website",
+	},
+	twitter: {
+		title: title,
+		description: description,
+		card: "summary_large_image",
+		images: [
+			{
+				url: "/images/parla-logo-twitter-image.png",
+				width: 1200,
+				height: 1200,
+				alt: "Parla Logo",
+			},
+		],
+	},
 };
 
 export default function RootLayout({
