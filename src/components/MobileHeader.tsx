5be2a2cfef9948ed3fa0546c5f2ab874f3bac40e
Jonas Jaszkowic <jonas.jaszkowic@ts.berlin>
2024-01-17 13:50:22 +0100

    feat: logo in mobile header

--- src/components/MobileHeader.tsx
+++ src/components/MobileHeader.tsx
@@ -17,7 +17,13 @@ export default function MobileHeader({
 				>
 					<HamburgerMenuIcon className="w-[20px] h-[20px]"></HamburgerMenuIcon>
 				</button>
-				<div className="italic font-bold">Parla</div>
+				<a href="/" className="w-full flex justify-center">
+					<img
+						alt="Parla Logo"
+						src="/images/parla-logo-v1.svg"
+						className="w-[30%] sm:w-[20%] md:w[15%] py-3"
+					/>
+				</a>
 				<button onClick={openSplashScreen}>
 					<InfoCircledIcon className="w-6 h-6 "></InfoCircledIcon>
 				</button>
