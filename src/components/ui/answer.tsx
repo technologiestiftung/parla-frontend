83c1f9034d16a461f026d777b3aa073051f5dc54
Jonas Jaszkowic <jonas.jaszkowic@ts.berlin>
2024-01-18 12:23:25 +0100

    fix: markdown list css (#81)
    * fix: markdown list css
    
    * fix: spacing between paragraphs
    
    * fix: heading styling in markdown

--- src/components/ui/answer.tsx
+++ src/components/ui/answer.tsx
@@ -2,7 +2,7 @@ import { DocumentSearchResponse } from "@/lib/common";
 import { getDocumentsCount } from "@/lib/get-documents-count";
 import { texts } from "@/lib/texts";
 import { cn } from "@/lib/utils";
-import { ReactNode, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import ReactMarkdown from "react-markdown";
 import AnswerLoadingSkeleton from "./textLoadingSkeleton";
 
@@ -55,7 +55,7 @@ function Answer(props: AnswerProps): JSX.Element {
 				{generatedAnswer && (
 					<>
 						<div data-testid="generated-answer">
-							<ReactMarkdown className="leading-6 bg-white p-4 rounded-lg shadow-md">
+							<ReactMarkdown className="markdown-container leading-6 bg-white p-4 rounded-lg shadow-md space-y-4">
 								{generatedAnswer}
 							</ReactMarkdown>
 						</div>
@@ -69,6 +69,7 @@ function Answer(props: AnswerProps): JSX.Element {
 						>
 							<ReactMarkdown
 								className={cn(
+									`markdown-container`,
 									`text-slate-500 transition-colors group-hover:text-slate-900`,
 									`group-focus-visible:text-slate-900 prose-strong:text-inherit`,
 									`group-focus-visible:ring-2 group-focus-visible:rounded-sm`,
