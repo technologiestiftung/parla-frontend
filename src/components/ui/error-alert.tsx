7e4820527745bfc94c46475820415d93415374df
Jonas Jaszkowic <jonas.jaszkowic@ts.berlin>
2024-01-23 15:30:13 +0100

    feat: orange error

--- src/components/ui/error-alert.tsx
+++ src/components/ui/error-alert.tsx
@@ -12,11 +12,15 @@ export const ErrorAlert: FC<ErrorAlertProps> = ({ callback, error }) => {
 			className="max-w-3xl mx-auto bg-orange-200 rounded-md p-3 text-sm mt-2 flex justify-between"
 			data-testid="error-alert"
 		>
-			<div className="flex gap-1 items-center">
+			<div className="flex gap-1 items-center text-orange-700">
 				<ExclamationTriangleIcon></ExclamationTriangleIcon>
 				{error}
 			</div>
-			<button onClick={callback} data-testid="reload-icon">
+			<button
+				onClick={callback}
+				data-testid="reload-icon"
+				className="text-orange-700"
+			>
 				<ReloadIcon></ReloadIcon>
 			</button>
 		</div>
