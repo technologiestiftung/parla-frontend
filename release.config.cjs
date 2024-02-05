9882863d84f9387c510acbb2159409862077db91
Fabian Morón Zirfas <fabian.moron-zirfas@ts.berlin>
2024-02-05 09:44:37 +0100

    Staging to main (#94)
    * ci(staging): Run tests on staging
    
    * ci(release): se bot needs write permissions
    
    * ci(release): Merge release from main to staging

--- release.config.cjs
+++ release.config.cjs
@@ -2,4 +2,10 @@ module.exports = {
 	extends: "@technologiestiftung/semantic-release-config",
 	npmPublish: false,
 	branches: [{ name: "main" }],
+	plugins: [
+		"@saithodev/semantic-release-backmerge",
+		{
+			backmergeBranches: ["staging"],
+		},
+	],
 };
